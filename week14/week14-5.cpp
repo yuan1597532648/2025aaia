82a3106d5af1603bc68beaea9956bf0cc721016f
yuan1597532648 <wayne20051105@gmail.com>
2025-05-23 12:10:21 +0800

    week14

---
+++ week14/week14-5.cpp
@@ -0,0 +1,17 @@
+import processing.sound.*;
+SoundFile music, sword, monkey, intro;
+void setup() {
+  size(640, 360);
+  music = new SoundFile(this, "In Game Music.mp3");
+  sword = new SoundFile(this, "sword slash.mp3");
+  monkey = new SoundFile(this, "monkey 1.mp3");
+  intro = new SoundFile(this, "Intro Song_Final.mp3");
+  music.play();
+}
+void mousePressed(){
+  if(mouseButton==LEFT) sword.play();
+  else monkey.play();
+}
+void draw() {
+
+}
